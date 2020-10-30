4673a190ecf89d4ee3859a9c44d2bad7826d54ab
Ezra <ezracook0@gmail.com>
2020-10-29 19:29:57 -0500

    update

--- runme.sh
+++ runme.sh
@@ -6,3 +6,9 @@ cd ..
 cd models
 cd linear_models
 python3 linear_model_feature_engineering.py
+cd ..
+cd tree_based_models
+python3 feature_engineering.py
+cd ..
+cd FFNN
+python3 feature_engineering.py
